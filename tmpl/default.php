cc1ee423f7e259e1d257418f061c9bfab38c76cd
Micha Buchholz <mail@lean-stack.rocks>
2025-12-12 00:02:39 +0100

    feat: add OmU category and use language property for categorization

--- tmpl/default.php
+++ tmpl/default.php
@@ -22,12 +22,22 @@ if (empty($events) || !is_array($events)) {
     return;
 }
 
-// Separate events into 3D and 2D
+// Separate events into 3D, 2D, OmU, and OV
 $events3D = [];
 $events2D = [];
+$eventsOmU = [];
+$eventsOV = [];
 
 foreach ($events as $id => $event) {
-    if (!empty($event->is3D)) {
+    // Check language property for OmU or OV
+    $isOmU = !empty($event->language) && stripos($event->language, 'OmU') !== false;
+    $isOV = !empty($event->language) && stripos($event->language, 'OV') !== false;
+
+    if ($isOmU) {
+        $eventsOmU[$id] = $event;
+    } elseif ($isOV) {
+        $eventsOV[$id] = $event;
+    } elseif (!empty($event->is3D)) {
         $events3D[$id] = $event;
     } else {
         $events2D[$id] = $event;
@@ -58,8 +68,48 @@ foreach ($events as $id => $event) {
 
     <?php if (!empty($events2D)) : ?>
         <h3>Aktuell in 2D</h3>
-        <div class="row g-3">
+        <div class="row g-3 mb-4">
             <?php foreach ($events2D as $id => $event) : ?>
+                <?php
+                $detailRoute = Route::_('index.php?option=com_cinetixx&view=event&event_id=' . $id);
+                ?>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <a href="<?= $detailRoute ?>" class="d-block">
+                        <?php if (!empty($event->poster)) : ?>
+                            <img src="<?= $event->poster ?>"
+                                 alt="<?= htmlspecialchars($event->title) ?>"
+                                 class="img-fluid rounded">
+                        <?php endif; ?>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (!empty($eventsOmU)) : ?>
+        <h3>Aktuell in OmU</h3>
+        <div class="row g-3 mb-4">
+            <?php foreach ($eventsOmU as $id => $event) : ?>
+                <?php
+                $detailRoute = Route::_('index.php?option=com_cinetixx&view=event&event_id=' . $id);
+                ?>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <a href="<?= $detailRoute ?>" class="d-block">
+                        <?php if (!empty($event->poster)) : ?>
+                            <img src="<?= $event->poster ?>"
+                                 alt="<?= htmlspecialchars($event->title) ?>"
+                                 class="img-fluid rounded">
+                        <?php endif; ?>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (!empty($eventsOV)) : ?>
+        <h3>Aktuell in OV</h3>
+        <div class="row g-3">
+            <?php foreach ($eventsOV as $id => $event) : ?>
                 <?php
                 $detailRoute = Route::_('index.php?option=com_cinetixx&view=event&event_id=' . $id);
                 ?>
